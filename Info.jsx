5e9651709b0e688050e86725599ab7083fa143f1
manju222 <62828730+manju222@users.noreply.github.com>
2021-06-27 16:40:52 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Info(){
+  return(
+    
+    <div class = "note">
+    <h1>JavaScript and React.Js </h1>
+    <p>
+      A basic web dev using React Js Bootcamp.
+      It was an amazing experience. Thank you to the Sir.
+    </p>
+    </div>
+    
+
+  );
+
+}
+export default Info;
